d9bdb7db70a729c411a80630b9e9e4a3ae552a64
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2023-02-07 18:04:50 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const newHeader = document.createElement("h1");
+document.body.append(newHeader);
+newHeader.id = "victory";
+newHeader.textContent="TAIWO is the champion";
+document.body.append(newHeader);
